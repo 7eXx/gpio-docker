1d8967c6152bf1352e2d7f3465b2fa1a8f269617
mark.texx <marco.tex@live.com>
2022-11-09 22:15:08 +0100

    fixed platform for docker build process.

--- build-push.sh
+++ build-push.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+set -xe
+
 [ -f .env ] && . ./.env
 [ -f .env.local ] && . ./.env.local
 
 cat ./.docker_pass | docker login -u $USERNAME --password-stdin
 
-docker build -t $full_image_name .
+full_image_name=${USERNAME}/${DOCKER_IMAGE}:${DOCKER_IMAGE_TAG}
 
-docker push $full_image_name 
+docker build --platform linux/arm/v6 -t ${full_image_name} .
+
+docker push ${full_image_name} 
 
 docker logout
